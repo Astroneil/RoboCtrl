94dc351a942558ac8df8cfe85896d7c2c4511b3a
Neil Isenor <neil.isenor@gmail.com>
2018-07-26 23:46:40 -0300

    minor bug fixes, grab left image only for click_pos

--- RoboCtrl/static/js/nova.js
+++ RoboCtrl/static/js/nova.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  $("img").on("click", function(event) {
+  $("#main_feed").on("click", function(event) {
     var width = this.clientWidth;
     var height = this.clientHeight;
     var x = event.pageX - this.offsetLeft;
     var y = event.pageY - this.offsetTop;
     var percent_x_calc = ((event.pageX - this.offsetLeft) / this.clientWidth) * 100;
-    var percent_y_calc = ((event.pageY - this.offsetTop) / this.clientWidth) * 100;
+    var percent_y_calc = ((event.pageY - this.offsetTop) / this.clientHeight) * 100;
     var percent_x = percent_x_calc.toFixed(2);
     var percent_y = percent_y_calc.toFixed(2);
       $.getJSON('/click_pos', {
